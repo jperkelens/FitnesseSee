a79b1a97b4839016fe97599ab914cdf902bc929e
JP Erkelens <jperkelens@gmail.com>
2010-03-18 12:59:19 -0400

    added files

---
+++ src/main/java/FitnesseSee/FitnesseResults.java
@@ -0,0 +1,11 @@
+package FitnesseSee;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: JanP
+ * Date: Mar 16, 2010
+ * Time: 4:51:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FitnesseResults extends TestResult {
+}
